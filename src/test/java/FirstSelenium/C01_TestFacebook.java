5c0b3387067259118885f4129fe8c6210bcc2406
Melisa Canbaz <melisa.canbazz@gmail.com>
2023-02-13 22:51:04 -0500

    first commit in the master branch

--- src/test/java/FirstSelenium/C01_TestFacebook.java
+++ src/test/java/FirstSelenium/C01_TestFacebook.java
@@ -1,4 +1,19 @@
 package FirstSelenium;
 
 public class C01_TestFacebook {
+
+    public static void main(String[] args) {
+        System.out.println("hellooo");
+
+         /*
+    Saturday Project (Selenium 1st Project)
+Goal of the project: As an Automation tester, you are going to visit Facebook page, manually test the sign-up functionality. Then automate your test cases and run your automation.
+Step 1- Create your user stories for each part of the sign-up functionality Step 2- Based on your user stories create your test cases and test scenarios Step 3- Based on your test cases start the test execution manually
+Step 4- Write your codes on Maven Project and automate your test cases.
+Summary of the Task :
+// Navigate https://www.facebook.com/ // Click on Create New Account button // Fill the form
+// Get text for Sign Up button
+     */
+    }
+
 }
